2cf0b357d827e085cf917cb33e6857e69a65eaab
Sebastian Romero <nakosume@gmail.com>
2023-09-29 08:00:49 -0500

    a

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,14 +4,15 @@ import { User } from "./entities/User"
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
+    host: "ep-proud-haze-26916324.us-east-2.aws.neon.fl0.io",
     port: 5432,
-    username: "nako",
-    password: "sume",
-    database: "test",
+    username: "fl0user",
+    password: "DSVKdikms4c9",
+    database: "JUJU",
     synchronize: true,
-    logging: false,
+    logging: true,
     entities: [User],
     migrations: [],
     subscribers: [],
+    ssl: { rejectUnauthorized: false },
 })
